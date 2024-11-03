37be82ffa5ff6da509e84449625e8db359d7dfa5
Arseniy2059 <semyashulkovy64@mail.ru>
2024-11-03 05:18:13 +0300

    Update знания 13.py

--- знания 13.py
+++ знания 13.py
@@ -2,7 +2,7 @@ e = input("my_string")
 print(len("my_string"))
 print("my_string".upper())
 print("my_string".lower())
-print("my_string".replace(' ', ''))
+print("m y _ s t r i n g".replace(' ', ''))
 a = "my_string"
 print(a[0])
 print(a[-1])

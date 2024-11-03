e5cafc71e64629e709679b5964166fb9328a64f3
Arseniy2059 <semyashulkovy64@mail.ru>
2024-11-03 05:16:38 +0300

    Add files via upload

---
+++ знания 13.py
@@ -0,0 +1,8 @@
+e = input("my_string")
+print(len("my_string"))
+print("my_string".upper())
+print("my_string".lower())
+print("my_string".replace(' ', ''))
+a = "my_string"
+print(a[0])
+print(a[-1])
